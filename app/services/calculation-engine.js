d36942084ec5129433b8050bcdf6bef2c04fd2d8
bmcbrayer@precisionlender.com <bmcbrayer@precisionlender.com>
2016-12-07 11:37:08 -0500

    Created deploy branch

--- app/services/calculation-engine.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Service.extend({
-  absoluteDifference(numberOne, numberTwo) {
-    return Math.abs(parseInt(numberOne) - parseInt(numberTwo));
-  }
-});
